376cddc79ae5b65ce453229fd49bdea5d3f901b8
ManhHung1501 <hungnm2@ftech.ai>
2024-12-24 15:38:39 +0700

    update

--- dags/ethereumetl_airflow/build_export_dag.py
+++ dags/ethereumetl_airflow/build_export_dag.py
@@ -112,3 +112,226 @@ def build_export_dag(
         with open(os.path.join(tempdir, "blocks_meta.txt")) as block_range_file:
             block_range = block_range_file.read()
             start_block, end_block = block_range.split(",")
+        
+        return int(start_block), int(end_block)
+
+    def export_blocks_and_transactions_command(logical_date, provider_uri, **kwargs):
+        with TemporaryDirectory(dir=TEMP_DIR) as tempdir:
+            start_block, end_block = get_block_range(tempdir, logical_date, provider_uri)
+
+            logging.info('Calling export_blocks_and_transactions({}, {}, {}, {}, {}, ...)'.format(
+                start_block, end_block, export_batch_size, provider_uri, export_max_workers))
+
+            export_blocks_and_transactions.callback(
+                start_block=start_block,
+                end_block=end_block,
+                batch_size=export_batch_size,
+                provider_uri=provider_uri,
+                max_workers=export_max_workers,
+                blocks_output=os.path.join(tempdir, "blocks.csv"),
+                transactions_output=os.path.join(tempdir, "transactions.csv"),
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "blocks_meta.txt"), export_path("blocks_meta", logical_date)
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "blocks.csv"), export_path("blocks", logical_date)
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "transactions.csv"), export_path("transactions", logical_date)
+            )
+        
+    def export_receipts_and_logs_command(logical_date, provider_uri, **kwargs):
+        with TemporaryDirectory(dir=TEMP_DIR) as tempdir:
+            copy_from_export_path(
+                export_path("transactions", logical_date), os.path.join(tempdir, "transactions.csv")
+            )
+
+            logging.info('Calling extract_csv_column(...)')
+            extract_field.callback(
+                input=os.path.join(tempdir, "transactions.csv"),
+                output=os.path.join(tempdir, "transaction_hashes.txt"),
+                field="hash",
+            )
+
+            logging.info('Calling export_receipts_and_logs({}, ..., {}, {}, ...)'.format(
+                export_batch_size, provider_uri, export_max_workers))
+            export_receipts_and_logs.callback(
+                batch_size=export_batch_size,
+                transaction_hashes=os.path.join(tempdir, "transaction_hashes.txt"),
+                provider_uri=provider_uri,
+                max_workers=export_max_workers,
+                receipts_output=os.path.join(tempdir, "receipts.csv"),
+                logs_output=os.path.join(tempdir, "logs.csv"),
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "receipts.csv"), export_path("receipts", logical_date)
+            )
+            copy_to_export_path(os.path.join(tempdir, "logs.csv"), export_path("logs", logical_date))
+
+    def extract_contracts_command(logical_date, **kwargs):
+        with TemporaryDirectory(dir=TEMP_DIR) as tempdir:
+            copy_from_export_path(
+                export_path("traces", logical_date), os.path.join(tempdir, "traces.csv")
+            )
+
+            logging.info('Calling extract_contracts(..., {}, {})'.format(
+                export_batch_size, export_max_workers
+            ))
+            extract_contracts.callback(
+                traces=os.path.join(tempdir, "traces.csv"),
+                output=os.path.join(tempdir, "contracts.csv"),
+                batch_size=export_batch_size,
+                max_workers=export_max_workers,
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "contracts.csv"), export_path("contracts", logical_date)
+            )
+
+    def extract_tokens_command(logical_date, provider_uri, **kwargs):
+        with TemporaryDirectory(dir=TEMP_DIR) as tempdir:
+            copy_from_export_path(
+                export_path("contracts", logical_date), os.path.join(tempdir, "contracts.csv")
+            )
+
+            logging.info('Calling extract_tokens(..., {}, {})'.format(export_max_workers, provider_uri))
+            extract_tokens.callback(
+                contracts=os.path.join(tempdir, "contracts.csv"),
+                output=os.path.join(tempdir, "tokens.csv"),
+                max_workers=export_max_workers,
+                provider_uri=provider_uri,
+                values_as_strings=True,
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "tokens.csv"), export_path("tokens", logical_date)
+            )
+
+    def extract_token_transfers_command(logical_date, **kwargs):
+        with TemporaryDirectory(dir=TEMP_DIR) as tempdir:
+            copy_from_export_path(
+                export_path("logs", logical_date), os.path.join(tempdir, "logs.csv")
+            )
+
+            logging.info('Calling extract_token_transfers(..., {}, ..., {})'.format(
+                export_batch_size, export_max_workers
+            ))
+            extract_token_transfers.callback(
+                logs=os.path.join(tempdir, "logs.csv"),
+                batch_size=export_batch_size,
+                output=os.path.join(tempdir, "token_transfers.csv"),
+                max_workers=export_max_workers,
+                values_as_strings=True,
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "token_transfers.csv"),
+                export_path("token_transfers", logical_date),
+            )
+
+    def export_traces_command(logical_date, provider_uri, **kwargs):
+        with TemporaryDirectory(dir=TEMP_DIR) as tempdir:
+            start_block, end_block = get_block_range(tempdir, logical_date, provider_uri)
+
+            logging.info('Calling export_traces({}, {}, {}, ...,{}, {}, {}, {})'.format(
+                start_block, end_block, export_batch_size, export_max_workers, provider_uri,
+                export_genesis_traces_option, export_daofork_traces_option
+            ))
+            export_traces.callback(
+                start_block=start_block,
+                end_block=end_block,
+                batch_size=export_batch_size,
+                output=os.path.join(tempdir, "traces.csv"),
+                max_workers=export_max_workers,
+                provider_uri=provider_uri,
+                genesis_traces=export_genesis_traces_option,
+                daofork_traces=export_daofork_traces_option,
+            )
+
+            copy_to_export_path(
+                os.path.join(tempdir, "traces.csv"), export_path("traces", logical_date)
+            )
+
+    def add_export_task(toggle, task_id, python_callable, dependencies=None):
+        if toggle:
+            operator = PythonOperator(
+                task_id=task_id,
+                python_callable=python_callable,
+                execution_timeout=timedelta(hours=15),
+                dag=dag,
+            )
+            if dependencies is not None and len(dependencies) > 0:
+                for dependency in dependencies:
+                    if dependency is not None:
+                        dependency >> operator
+            return operator
+        else:
+            return None
+    
+    # Operators
+    def add_provider_uri_fallback_loop(python_callable, provider_uris):
+        """Tries each provider uri in provider_uris until the command succeeds"""
+
+        def python_callable_with_fallback(**kwargs):
+            for index, provider_uri in enumerate(provider_uris):
+                kwargs['provider_uri'] = provider_uri
+                try:
+                    python_callable(**kwargs)
+                    break
+                except Exception as e:
+                    if index < (len(provider_uris) - 1):
+                        logging.exception('An exception occurred. Trying another uri')
+                    else:
+                        raise e
+
+        return python_callable_with_fallback
+
+    export_blocks_and_transactions_operator = add_export_task(
+        export_blocks_and_transactions_toggle,
+        "export_blocks_and_transactions",
+        add_provider_uri_fallback_loop(export_blocks_and_transactions_command, provider_uris),
+    )
+
+    export_receipts_and_logs_operator = add_export_task(
+        export_receipts_and_logs_toggle,
+        "export_receipts_and_logs",
+        add_provider_uri_fallback_loop(export_receipts_and_logs_command, provider_uris),
+        dependencies=[export_blocks_and_transactions_operator],
+    )
+
+    extract_token_transfers_operator = add_export_task(
+        extract_token_transfers_toggle,
+        "extract_token_transfers",
+        extract_token_transfers_command,
+        dependencies=[export_receipts_and_logs_operator],
+    )
+
+    export_traces_operator = add_export_task(
+        export_traces_toggle,
+        "export_traces",
+        add_provider_uri_fallback_loop(export_traces_command, provider_uris_archival)
+    )
+
+    extract_contracts_operator = add_export_task(
+        extract_contracts_toggle,
+        "extract_contracts",
+        extract_contracts_command,
+        dependencies=[export_traces_operator],
+    )
+
+    extract_tokens_operator = add_export_task(
+        extract_tokens_toggle,
+        "extract_tokens",
+        add_provider_uri_fallback_loop(extract_tokens_command, provider_uris),
+        dependencies=[extract_contracts_operator],
+    )
+
+    return dag
+
+
+    
