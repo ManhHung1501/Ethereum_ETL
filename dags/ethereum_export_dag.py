67644451dd4d157035b769defd5b2f388d592b3d
ManhHung1501 <hungnm2@ftech.ai>
2024-12-25 15:37:47 +0700

    update

--- dags/ethereum_export_dag.py
+++ dags/ethereum_export_dag.py
@@ -11,8 +11,8 @@ DAG = build_export_dag(
         export_start_date='2024-12-23',
         export_max_workers=10,
         export_batch_size=10,
-        export_retries=5,
-        export_daofork_traces_option=False,
-        export_genesis_traces_option=False
+        export_retries=5
+        # export_daofork_traces_option=False,
+        # export_genesis_traces_option=False
     )
 )
